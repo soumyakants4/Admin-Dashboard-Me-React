1f3c80fcd96cb03c9640a97ff00e1c6624427c8f
Soumyakant Sahoo <soumyakant.sahoo@192.168.1.15>
2023-02-03 20:12:57 +0530

    tillUpdate

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,27 @@
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featured/Featured";
+import "./home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
