a8df1cfa719213a8b1084ed3ed83ce3f247ace99
Soumyakant Sahoo <soumyakant.sahoo@192.168.1.6>
2023-02-06 13:29:26 +0530

    commitDay2

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,7 +2,8 @@ import "./featured.scss"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import "react-circular-progressbar/dist/styles.css";
-
+import { KeyboardArrowUpOutlined } from "@mui/icons-material";
+import { KeyboardArrowDownOutlined } from "@mui/icons-material";
 const Featured = () => {
   return (
     <div className="featured">
@@ -18,6 +19,35 @@ const Featured = () => {
         <p className="amount">$420</p>
         <p className="desc">Previous transactions processing. Last payment may not be included
         </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <div className="resultAmount">
+                <KeyboardArrowDownOutlined fontSize="small" />
+                $10K
+                </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <div className="resultAmount">
+                <KeyboardArrowDownOutlined fontSize="small" />
+                $10K
+                </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <div className="resultAmount">
+                <KeyboardArrowDownOutlined fontSize="small" />
+                $10K
+                </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
