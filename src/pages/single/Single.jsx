aafa1c56b96b5b416dd26f8c942559e5783884c6
Soumyakant Sahoo <soumyakant.sahoo@192.168.1.6>
2023-02-07 22:17:01 +0530

    7 Feb 2023-3

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,6 +1,7 @@
 import "./single.scss"
-import Sidebar from "src/components/sidebar/Sidebar"
-import Navbar  from "src/components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
 
 const Single = () => {
   return (
@@ -8,7 +9,42 @@ const Single = () => {
       <Sidebar />
       <div className="singleContainer">
         <Navbar />
-        Single
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+            <img
+                src="https://www.freecodecamp.org/news/content/images/2021/03/Quincy-Larson-photo.jpg"
+                alt=""
+                className="itemImg"
+              /> 
+              <div className="details">
+                <h1 className="itemTitle">Yashraj</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email: </span>
+                  <span className="itemValue">rey.yash@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone: </span>
+                  <span className="itemValue">+91 9999999999</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address: </span>
+                  <span className="itemValue">Mantri Serenity, Bangalore</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country: </span>
+                  <span className="itemValue">India</span>
+                </div>
+                </div>           
+              </div>
+          </div>
+          <div className="right">
+            <Chart/>
+          </div>
+        </div>
+        <div className="bottom"></div>
       </div>
     </div>
   )
