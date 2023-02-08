8cf506f086fff95ce48984c38232b8cde74d46c3
Soumyakant Sahoo <soumyakant.sahoo@192.168.1.6>
2023-02-08 20:43:54 +0530

    8 Feb 2023-2

---
+++ src/formSource.js
@@ -0,0 +1,85 @@
+export const userInputs = [
+    {
+        id: 1,
+        label: "Username",
+        type: "text",
+        placeholder: "snow_jon99"
+    },
+    {
+        id: 2,
+        label: "First Name",
+        type: "text",
+        placeholder: "Jon"
+    },
+    {
+        id: 3,
+        label: "Last Name",
+        type: "text",
+        placeholder: "Snow"
+    },
+    {
+        id: 4,
+        label: "Phone",
+        type: "text",
+        placeholder: "+91 9034038485"
+    },
+    {
+        id: 5,
+        label: "Address",
+        type: "text",
+        placeholder: "Elton St. 2092"
+    },
+    {
+        id: 6,
+        label: "Country",
+        type: "text",
+        placeholder: "United States"
+    },
+    {
+        id: 7,
+        label: "Email",
+        type: "text",
+        placeholder: "snow_jon99@gmail.com"
+    },
+    {
+        id: 8,
+        label: "Password",
+        type: "password",
+        placeholder: ""
+    },
+];
+
+
+export const productInputs = [
+    {
+        id: 1,
+        label: "Product Name",
+        type: "text",
+        placeholder: "Apple MacBook Pro 13'"
+    },
+    {
+        id: 2,
+        label: "Description",
+        type: "text",
+        placeholder: "Description"
+    },
+    {
+        id: 3,
+        label: "Category",
+        type: "text",
+        placeholder: "Computers"
+    },
+    {
+        id: 4,
+        label: "Price",
+        type: "text",
+        placeholder: "100"
+    },
+    {
+        id: 5,
+        label: "Stock",
+        type: "text",
+        placeholder: "In Stock"
+    },
+
+];
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,71 @@
-import "./new.scss"
+import "./new.scss";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { DriveFolderUploadOutlined } from "@mui/icons-material";
 
 const New = () => {
   return (
-    <div>New</div>
-  )
-}
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add New User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt="avatar"
+            />
+          </div>
+          <div className="right"><form>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlined className="icon"/>
+                </label>
+                <input type="file" id="file" style={{display: "none"}}  />
+              </div>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder="snow_99"  />
+              </div>
+              <div className="formInput">
+                <label>Name</label>
+                <input type="text" placeholder="Jon"  />
+              </div>
+              <div className="formInput">
+                <label>Surname</label>
+                <input type="text" placeholder="Snow"  />
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="text" placeholder="+91 XXXXXXXXXX"  />
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" placeholder="Elton St. 20909"  />
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" placeholder="India"  />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="text" placeholder="snow.jon99@gmail.com"  />
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password" placeholder=""  />
+              </div>
+              <button>Upload</button>
+            </form>
+            </div>
+        </div>
+        test
+      </div>
+    </div>
+  );
+};
 
-export default New
+export default New;
